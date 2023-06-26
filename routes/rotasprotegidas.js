49037e29e55a0e310088eb618f573bd69086a842
GuilhermeAndreotti <gandreotti@alunos.utfpr.edu.br>
2023-06-25 22:38:18 -0300

    feat: carga automatica

--- routes/rotasprotegidas.js
+++ routes/rotasprotegidas.js
@@ -2,6 +2,7 @@ const express = require("express");
 const usuariobd = require("../models/usuariobd");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const jogobd = require("../models/jogobd");
 
 router.get('/principal', function(req, res, next){
     res.render('principal',{ token: req.query.token, usuario: req.usuario.id });
@@ -21,6 +22,24 @@ router.get('/cadastrartreino', function(req, res, next){
 
 router.get('/grafico', function(req, res, next){
   res.render('grafico',{ token: req.query.token, usuario: req.usuario.id});
+});
+
+router.get('/carga-automatica', async function(req, res, next){
+
+    const id = req.usuario.id;
+
+    const jogoCarga = [
+      { nome: 'Kill the Killer', modalidade: 'individual', descricao: 'Jogo em que você duela contra todo mundo. Ganha quem derrotar todos'},
+      { nome: 'Jogo do Rei', modalidade: 'equipe', descricao: 'Jogo em que você precisa eliminar o rei inimigo.'},
+      { nome: 'Jogo do x1', modalidade: 'individual', descricao: 'Você tira um x1 com todo mundo.'},
+      { nome: 'Rei Oculto', modalidade: 'equipe', descricao: 'Jogo em que você precisa eliminar o rei inimigo sem saber quem é o rei.'},
+      { nome: 'Armas do Poder', modalidade: 'equipe', descricao: 'Cada equipe tem uma arma com um poder especial.'},
+    ];
+
+    for(let i = 0; i < 5; i++){
+      await jogobd.cadastrarJogos(id, jogoCarga[i].nome, jogoCarga[i].modalidade, jogoCarga[i].descricao )
+    }
+
 });
 
 
